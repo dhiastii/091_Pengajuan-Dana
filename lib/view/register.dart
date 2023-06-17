3f38f45f0741dc9604f48208676f9b4c5749422b
dhiastii <dhiastiputri.jayanti@gmail.com>
2023-06-17 08:24:28 +0700

    declare variable to resgister

--- lib/view/register.dart
+++ lib/view/register.dart
@@ -1,1 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:pengajuan_dana/controller/auth_controller.dart';
+
+class Register extends StatelessWidget {
+  Register({super.key});
+
+//globalkey buat kontrol dan akses formstate
+//formstate buat validasi formulir di flutter
+  final formkey = GlobalKey<FormState>;
+  final authCtrl = AuthController();
+
+  @override
+  Widget build(BuildContext context) {
+    String? nama;
+    String? nim;
+    String? nohp;
+    String? jabatan;
+    String? divisi;
+    String? email;
+    String? password;
+
+    return Scaffold();
+  }
+}
