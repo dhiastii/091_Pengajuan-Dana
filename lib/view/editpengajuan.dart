845cee4c3f7b5f2945c298a3047d2825859e0be4
dhiastii <dhiastiputri.jayanti@gmail.com>
2023-06-21 21:12:40 +0700

    add new file view pengajuan

--- lib/view/editpengajuan.dart
+++ lib/view/editpengajuan.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:pengajuan_dana/view/addpengajuan.dart';
 
 class EditPengajuan extends StatefulWidget {
   const EditPengajuan({super.key});
@@ -11,6 +14,55 @@ class EditPengajuan extends StatefulWidget {
 class _EditPengajuanState extends State<EditPengajuan> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    String namak;
+    String tgl;
+    String desk;
+    String dana;
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Edit Pengajuan Dana'),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(children: [
+            TextFormField(
+              decoration: const InputDecoration(hintText: 'Nama Kegiatan'),
+              onChanged: (value) {
+                namak = value;
+              },
+            ),
+            TextFormField(
+              decoration: const InputDecoration(hintText: 'Tanggal Kegiatan'),
+              onChanged: (value) {
+                tgl = value;
+              },
+            ),
+            TextFormField(
+              decoration: const InputDecoration(hintText: 'Deskripsi Kegiatan'),
+              onChanged: (value) {
+                desk = value;
+              },
+            ),
+            TextFormField(
+              decoration: const InputDecoration(hintText: 'Pengajuan Dana'),
+              onChanged: (value) {
+                dana = value;
+              },
+            ),
+            TextFormField(
+              decoration: const InputDecoration(hintText: 'Upload Proposal'),
+              onChanged: (value) {
+                dana = value;
+              },
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => AddPengajuan()));
+              },
+              child: const Text('Save'),
+            ),
+          ]),
+        ));
   }
 }
