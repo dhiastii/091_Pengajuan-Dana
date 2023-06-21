d1af6a9ab31cc65cf776daf11db9ab2ecc10976b
dhiastii <dhiastiputri.jayanti@gmail.com>
2023-06-21 20:38:46 +0700

    add statefulwidget on editpengajuan

---
+++ lib/view/editpengajuan.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class EditPengajuan extends StatefulWidget {
+  const EditPengajuan({super.key});
+
+  @override
+  State<EditPengajuan> createState() => _EditPengajuanState();
+}
+
+class _EditPengajuanState extends State<EditPengajuan> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
