e17cb0c8482f89d47ad2707fa511767a874ed48a
dhiastii <dhiastiputri.jayanti@gmail.com>
2023-06-22 21:11:09 +0700

    add logo and edit position on add pengajuan

--- lib/view/addpengajuan.dart
+++ lib/view/addpengajuan.dart
@@ -15,50 +15,78 @@ class AddPengajuan extends StatelessWidget {
     String desk;
     String dana;
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Add Pengajuan Dana'),
+        // appBar: AppBar(
+        //   title: Text('Add Pengajuan Dana'),
+        // ),
+        body: Stack(children: <Widget>[
+      Positioned(
+          top: 1,
+          left: 130,
+          child: Image.asset(
+            'assets/images/logoatas.jpg',
+            width: 150,
+            height: 150,
+          )),
+      Positioned(
+        top: 120,
+        left: 65,
+        child: Text(
+          'Formulir Pengajuan Dana',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Color.fromRGBO(0, 0, 0, 1),
+            fontFamily: 'Arsenal',
+            fontSize: 24,
+            letterSpacing: 0,
+            fontWeight: FontWeight.normal,
+            height: 1,
+          ),
         ),
-        body: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(children: [
-            TextFormField(
-              decoration: const InputDecoration(hintText: 'Nama Kegiatan'),
-              onChanged: (value) {
-                namak = value;
-              },
-            ),
-            TextFormField(
-              decoration: const InputDecoration(hintText: 'Tanggal Kegiatan'),
-              onChanged: (value) {
-                tgl = value;
-              },
-            ),
-            TextFormField(
-              decoration: const InputDecoration(hintText: 'Deskripsi Kegiatan'),
-              onChanged: (value) {
-                desk = value;
-              },
-            ),
-            TextFormField(
-              decoration: const InputDecoration(hintText: 'Pengajuan Dana'),
-              onChanged: (value) {
-                dana = value;
-              },
-            ),
-            TextFormField(
-              decoration: const InputDecoration(hintText: 'Upload Proposal'),
-              onChanged: (value) {
-                dana = value;
-              },
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => EditPengajuan()));
-              },
-              child: const Text('Tambah Pengajuan'),
-            ),
-          ]),
-        ));
+      ),
+      Positioned(
+        top: 180,
+        left: 30,
+        right: 50,
+        child: Column(children: [
+          TextFormField(
+            decoration: const InputDecoration(hintText: 'Nama Kegiatan'),
+            onChanged: (value) {
+              namak = value;
+            },
+          ),
+          TextFormField(
+            decoration: const InputDecoration(hintText: 'Tanggal Kegiatan'),
+            onChanged: (value) {
+              tgl = value;
+            },
+          ),
+          TextFormField(
+            decoration: const InputDecoration(hintText: 'Deskripsi Kegiatan'),
+            onChanged: (value) {
+              desk = value;
+            },
+          ),
+          TextFormField(
+            decoration: const InputDecoration(hintText: 'Pengajuan Dana'),
+            onChanged: (value) {
+              dana = value;
+            },
+          ),
+          TextFormField(
+            decoration: const InputDecoration(hintText: 'Upload Proposal'),
+            onChanged: (value) {
+              dana = value;
+            },
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => EditPengajuan()));
+            },
+            child: const Text('Tambah Pengajuan'),
+          ),
+        ]),
+      )
+    ]));
   }
 }
