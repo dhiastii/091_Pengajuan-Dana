d18d628ab4442c2a28cb6ae995e0e71ac797561f
dhiastii <dhiastiputri.jayanti@gmail.com>
2023-06-22 22:16:35 +0700

    add tampilan keputusan

---
+++ lib/view/keputusan.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class KeputusanProdi extends StatefulWidget {
+  const KeputusanProdi({super.key});
+
+  @override
+  State<KeputusanProdi> createState() => _KeputusanProdiState();
+}
+
+class _KeputusanProdiState extends State<KeputusanProdi> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
