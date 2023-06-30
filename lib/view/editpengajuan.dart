972d4c07883d4baec8410209a27bf4fd450e1504
dhiastii <dhiastiputri.jayanti@gmail.com>
2023-06-30 23:26:34 +0700

    edit bagian edit pengajuan

--- lib/view/editpengajuan.dart
+++ lib/view/editpengajuan.dart
@@ -2,96 +2,111 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:pengajuan_dana/controller/list_controller.dart';
+import 'package:pengajuan_dana/model/list_model.dart';
 import 'package:pengajuan_dana/view/addpengajuan.dart';
 import 'package:pengajuan_dana/view/cobalist.dart';
 
 class EditPengajuan extends StatefulWidget {
-  const EditPengajuan({super.key});
+  const EditPengajuan({
+    Key? key,
+    this.id,
+    this.namak,
+    this.tgl,
+    this.desk,
+    this.dana,
+  }) : super(key: key);
+
+  final String? id;
+  final String? namak;
+  final String? tgl;
+  final String? desk;
+  final String? dana;
 
   @override
   State<EditPengajuan> createState() => _EditPengajuanState();
 }
 
 class _EditPengajuanState extends State<EditPengajuan> {
+  var listController = ListController();
+
+  final formkey = GlobalKey<FormState>();
+
+  //final List<DocumentSnapshot> data = snapshot.data!;
+
+  String? newnamak;
+  String? newtgl;
+  String? newdesk;
+  String? newdana;
+
   @override
   Widget build(BuildContext context) {
-    String namak;
-    String tgl;
-    String desk;
-    String dana;
     return Scaffold(
-        // appBar: AppBar(
-        //   title: Text('Edit Pengajuan Dana'),
-        // ),
-        body: Stack(children: <Widget>[
-      Positioned(
-          top: 1,
-          left: 130,
-          child: Image.asset(
-            'assets/images/logoatas.jpg',
-            width: 150,
-            height: 150,
-          )),
-      Positioned(
-        top: 120,
-        left: 85,
-        child: Text(
-          'Edit Pengajuan Dana',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Color.fromRGBO(0, 0, 0, 1),
-            fontFamily: 'Arsenal',
-            fontSize: 24,
-            letterSpacing: 0,
-            fontWeight: FontWeight.normal,
-            height: 1,
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Form(
+          key: formkey,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(hintText: 'Nama Kegiatan : '),
+                onSaved: (value) {
+                  newnamak = value;
+                },
+                initialValue: widget.namak,
+              ),
+              TextFormField(
+                decoration:
+                    const InputDecoration(hintText: 'Tanggal Kegiatan : '),
+                onSaved: (value) {
+                  newtgl = value;
+                },
+                initialValue: widget.tgl,
+              ),
+              TextFormField(
+                decoration:
+                    const InputDecoration(hintText: 'Deksripsi Kegiatan : '),
+                onSaved: (value) {
+                  newdesk = value;
+                },
+                initialValue: widget.desk,
+              ),
+              TextFormField(
+                decoration:
+                    const InputDecoration(hintText: 'Pengajuan Dana : Rp'),
+                onSaved: (value) {
+                  newdana = value;
+                },
+                initialValue: widget.dana,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (formkey.currentState!.validate()) {
+                    formkey.currentState!.save();
+                    ListModel ls = ListModel(
+                        id: widget.id,
+                        namak: newnamak!.toString(),
+                        tgl: newtgl!.toString(),
+                        desk: newdesk!.toString(),
+                        dana: newdana!.toString());
+                    listController.updateList(ls);
+                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        content: Text('Pengajuan berhasil di update')));
+
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const ListPengajuan2(),
+                      ),
+                    );
+                  }
+                },
+                child: const Text('Edit Pengajuan'),
+              )
+            ],
           ),
         ),
       ),
-      Positioned(
-        top: 180,
-        left: 30,
-        right: 50,
-        child: Column(children: [
-          TextFormField(
-            decoration: const InputDecoration(hintText: 'Nama Kegiatan'),
-            onChanged: (value) {
-              namak = value;
-            },
-          ),
-          TextFormField(
-            decoration: const InputDecoration(hintText: 'Tanggal Kegiatan'),
-            onChanged: (value) {
-              tgl = value;
-            },
-          ),
-          TextFormField(
-            decoration: const InputDecoration(hintText: 'Deskripsi Kegiatan'),
-            onChanged: (value) {
-              desk = value;
-            },
-          ),
-          TextFormField(
-            decoration: const InputDecoration(hintText: 'Pengajuan Dana'),
-            onChanged: (value) {
-              dana = value;
-            },
-          ),
-          TextFormField(
-            decoration: const InputDecoration(hintText: 'Upload Proposal'),
-            onChanged: (value) {
-              dana = value;
-            },
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => ListPengajuan2()));
-            },
-            child: const Text('Save'),
-          ),
-        ]),
-      )
-    ]));
+    );
   }
 }
